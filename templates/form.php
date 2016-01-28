ac9ee98e43181f609157dcfc94eff2f8104ea468
Monia <monia.benlahcen86@gmail.com>
2016-01-29 00:03:57 +0100

    Suppression du code script mis en header. Issue: le formulaire ne s'affiche pas entièrement

--- templates/form.php
+++ templates/form.php
@@ -16,16 +16,6 @@
         }
 
     </style>
-    <script src="js/jquery-1.12.0.js">
-        $(function(){
-            $.getJSON("js/wine.json",
-                    function(wine){
-                        console.log(wine);
-                    }
-            );
-        });
-    </script>
-
 </head>
 <body>
 <div>
@@ -40,27 +30,36 @@
     </ul>
 </div>
 <div>
-    <form action="<?= $app->urlFor('ajoutWines');  ?>" id="formVin" method="post">
-    <input type="reset" id="btReset" name="btReset" value="New">
-    <label for="nameVin">Name:</label>
-    <input type="text" id="nameVin" name="name" maxlength="50"/>
-    <label for="grapesVin">Grapes:</label>
-    <input type="text" id="grapesVin" name="grapes" maxlength="50"/>
-    <label for="countryVin">Country:</label>
-    <input type="text" id="countryVin" name="country" maxlength="50"/>
-    <label for="regionVin">Region:</label>
-    <input type="text" id="regionVin" name="region" maxlength="50"/>
-    <label for="yearVin">Year:</label>
-    <input type="number" id="yearVin" name="year" min="1900" max="2100"/>
-    <label for="description">Description :</label>
-    <textarea id="description" name="description" rows="10" cols="25" maxlength="500"></textarea>
-    <button type="submit" id="btSubmit" name="btSubmit">Save</button>
-    <button type="button" id="btDelete" name="btDelete">Delete</button>
-</form>
+    <!--<form action="<?= $app->urlFor('ajoutWines');  ?>" id="formVin" method="post">--> <!--TODO url PUT-->
+        <form action="#" id="formVin"> <!--En attendant URL post fait -->
+        <input type="reset" id="btReset" name="btReset" value="New">
+        <label for="nameVin">Name:</label>
+        <input type="text" id="nameVin" name="name" maxlength="50"/>
+        <label for="grapesVin">Grapes:</label>
+        <input type="text" id="grapesVin" name="grapes" maxlength="50"/>
+        <label for="countryVin">Country:</label>
+        <input type="text" id="countryVin" name="country" maxlength="50"/>
+        <label for="regionVin">Region:</label>
+        <input type="text" id="regionVin" name="region" maxlength="50"/>
+        <label for="yearVin">Year:</label>
+        <input type="number" id="yearVin" name="year" min="1900" max="2100"/>
+        <label for="description">Description :</label>
+        <textarea id="description" name="description" rows="10" cols="25" maxlength="500"></textarea>
+        <button type="submit" id="btSubmit" name="btSubmit">Save</button>
+        <button type="button" id="btDelete" name="btDelete">Delete</button>
+    </form>
 </div>
 <script src="../js/jquery-1.12.0.js"></script>
 <script>
-    //Ajax
+
+    $(document).ready(function() {
+        //Ajax
+        $.ajax("./listing.php",{// Lancement d'Ajax en reliant la source du fichier des données
+            success: function(response){
+                $("#listeVin").html(response).show();
+            }
+        });
+    });
 </script>
 </body>
 </html>
