b4797cb6c8427ab3d0dd9f414463fd4d4ffbd344
Monia <monia.benlahcen86@gmail.com>
2016-01-29 11:46:54 +0100

    Revert

--- templates/listing.php
+++ templates/listing.php
@@ -1,10 +1,8 @@
 
     <?php
-    for ($i = 0, $l = sizeof($donneeJSON); $l > $i; $i++){ //Mise en forme en fichier JSON afin de féliciter la récupération des données
-        if($i == 0)
-            echo "{</br>".$donneeJSON[0].",</br>";
-        elseif($i == $l)
-            echo $donneeJSON[$l].'}';
-        else
-            echo $donneeJSON[$i].",</br>";
+    echo '<ul id="listeJSON">';
+    for ($i = 0, $l = sizeof($donneeJSON); $l > $i; $i++){
+            echo '<li>'.$donneeJSON[$i].'</li>';
     }
+    echo '</ul>';
+
