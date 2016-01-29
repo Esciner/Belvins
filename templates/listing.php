ad8eecaf766de634746533fbff9064c7ca6b3276
Monia <monia.benlahcen86@gmail.com>
2016-01-29 12:50:13 +0100

    Récupération de la BD dans une variable json

--- templates/listing.php
+++ templates/listing.php
@@ -1,36 +1,6 @@
 <?php
-/*
     echo '<ul id="listeJSON">';
     for ($i = 0, $l = sizeof($donneeJSON); $l > $i; $i++){
             echo '<li>'.$donneeJSON[$i].'</li>';
     }
     echo '</ul>';
-*/
-?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Belvins- JSON BD</title>
-</head>
-<body>
-</body>
-<script scr="../js/jquery-1.12.0.js"></script>
-<script> // 2ème essai = récup bd dans une var js
-    $(function(){
-        var json = [
-            <?php
-            for ($i = 0, $l = sizeof($donneeJSON); $l > $i; $i++){
-                if($i != ($l-1))
-                    echo $donneeJSON[$i].",";
-                else
-                    echo $donneeJSON[$i];
-            }
-            ?>
-        ];
-        $("body").html(json);
-    });
-
-
-</script>
-</html>
