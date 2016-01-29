a63838f4bfe5fb5e57da26407ea2c9227debafd1
Esciner <benjamin.mallinus@gmail.com>
2016-01-29 18:39:22 +0100

    Rajout de la modification de vin

--- public/test.php
+++ public/test.php
@@ -139,6 +139,13 @@ $app->get('/formDelete',  function() use ($app){
     
 })->name('formDeSuppressionDeVin');
 
+//Page de formulaire de modif
+
+$app->get('/formModif',  function() use ($app){
+    $app->render('modif.php', compact('app'));
+    
+})->name('formDeModifDeVin');
+
 //Supprimer un vin avec l'id
 
 $app->delete('/api/wine/:id', function ($id) use ($app, $db){
@@ -147,6 +154,64 @@ $app->delete('/api/wine/:id', function ($id) use ($app, $db){
     echo "Record deleted successfully";
     
 })->name('deleteWinesById')->conditions(['id' => '[0-9]+']);
+
+//Modifier un vins
+
+$app->put('/api/wine/:id',  function($id) use ($app, $db){
+    if(!empty($_POST['name'])){
+        $name = $_POST['name'];
+    }else{
+        $name = '';
+    }
+    if(!empty($_POST['year'])){
+        $year = $_POST['year'];
+    }else{
+        $year = '';
+    }
+    if(!empty($_POST['grapes'])){
+        $grapes = $_POST['grapes'];
+    }else{
+        $grapes = '';
+    }
+    if(!empty($_POST['country'])){
+        $country = $_POST['country'];
+    }else{
+        $country = '';
+    }
+    if(!empty($_POST['region'])){
+        $region = $_POST['region'];
+    }else{
+        $region = '';
+    }
+    if(isset($_FILES['description'])){
+        //echo $_FILES['description'];
+        $description = fopen($_FILES['description']['tmp_name'], 'rb');
+    }else{
+        $description = $_FILES['description']['tmp_name'];
+    }
+    if(!empty($_FILES['picture'])){
+        $resultat = move_uploaded_file($_FILES['picture']['tmp_name'],'../pictures/' . $_FILES['picture']['name']);
+        if ($resultat) echo "Transfert réussi";
+        $picture = $_FILES['picture']['name'];
+    }else{
+        $picture = '';
+    }
+    
+    $stmt = $db->prepare("UPDATE wine SET name=?, year=?, grapes=?, country=?, region=?, description=?, picture=? where id=$id");
+    $stmt->bindParam(1, $name);
+    $stmt->bindParam(2, $year);
+    $stmt->bindParam(3, $grapes);
+    $stmt->bindParam(4, $country);
+    $stmt->bindParam(5, $region);
+    $stmt->bindParam(6, $description, PDO::PARAM_LOB);
+    $stmt->bindParam(7, $picture);
+    $stmt->execute();
+    //echo 'Ajout reussi';
+    //echo $description .'<br/>';
+    //echo $picture .'<br/>';
+    //print_r($_FILES);
+    //print_r($_POST);
+})->name('modifWines');
 
 //Rendu
 
