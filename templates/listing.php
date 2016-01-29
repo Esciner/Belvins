bd48058cc75e4b2fd0331041eee4cd2a381845a7
Monia <monia.benlahcen86@gmail.com>
2016-01-29 13:22:38 +0100

    restauration

--- templates/listing.php
+++ templates/listing.php
@@ -1,6 +1,6 @@
 <?php
-    echo '<ul id="listeJSON">';
-    for ($i = 0, $l = sizeof($donneeJSON); $l > $i; $i++){
-            echo '<li>'.$donneeJSON[$i].'</li>';
-    }
-    echo '</ul>';
+echo '<ul id="listeJSON">';
+for ($i = 0, $l = sizeof($donneeJSON); $l > $i; $i++) {
+    echo '<li>' . $donneeJSON[$i] . '</li>';
+}
+echo '</ul>';
